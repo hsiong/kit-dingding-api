7aa1ace2fe9c00a3496cdf065e9e3558db1c95f4
hsiong <peak1203@126.com>
2026-03-23 11:19:55 +0800

    feat: 新增钉钉机器人stream启动链路

--- src/main/java/com/example/dingding/ApiApplication.java
+++ src/main/java/com/example/dingding/ApiApplication.java
@@ -1,15 +1,26 @@
 package com.example.dingding;
 
+import com.alibaba.fastjson.JSONObject;
+import com.dingtalk.open.app.api.OpenDingTalkStreamClientBuilder;
+import com.dingtalk.open.app.api.security.AuthClientCredential;
+import com.example.dingding.config.dingding.DingdingConfig;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@Slf4j
 public class ApiApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
+	
+
+	
 	
 }
---
+++ src/main/java/com/example/dingding/service/BotCommandRunner.java
@@ -0,0 +1,53 @@
+package com.example.dingding.service;
+
+import com.alibaba.fastjson.JSONObject;
+import com.dingtalk.open.app.api.OpenDingTalkStreamClientBuilder;
+import com.dingtalk.open.app.api.security.AuthClientCredential;
+import com.example.dingding.config.dingding.DingdingConfig;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * 〈〉
+ *
+ * @author Hsiong
+ * @version 1.0.0
+ * @since 2026/3/23
+ */
+@Component
+@Slf4j
+public class BotCommandRunner implements CommandLineRunner {
+	
+	@Resource
+	private DingdingConfig dingdingConfig;
+	
+	/**
+	 * 启动机器人 - stream
+	 * https://open.dingtalk.com/document/development/introduction-to-stream-mode?spm=a2q3p.21071111.0.0.7a5d65eeKGZv7G
+	 */
+	private void runBot(){
+		try {
+			OpenDingTalkStreamClientBuilder
+				.custom()
+				.credential(new AuthClientCredential(dingdingConfig.getAppKey(), dingdingConfig.getAppSecret()))
+				//注册机器人监听器
+				.registerCallbackListener("${topic}", robotMessage -> {
+					log.info("receive robotMessage, {}", robotMessage);
+					//开发者根据自身业务需求，处理机器人回调
+					return new JSONObject();
+					
+				})
+				.build().start();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	
+	@Override
+	public void run(String... args) throws Exception {
+		runBot();
+	}
+}
